d8cf1e1c7043582163da79abc5f2bb98fa56ed46
Anurag-Solanki1 <solankimayank028.com>
2025-03-20 11:06:25 +0530

    Added Dev and Prod

--- src/main/java/com/example/AddressBook/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/AddressBook/Exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.AddressBook.Exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,41 +10,40 @@ import jakarta.validation.ConstraintViolationException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomErrorResponse> handleGeneralException(Exception ex) {
-        logger.error("An unexpected error occurred: {}", ex.getMessage(), ex);
+        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
         CustomErrorResponse errorResponse = new CustomErrorResponse("INTERNAL_SERVER_ERROR", "An unexpected error occurred.");
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        logger.error("Resource not found: {}", ex.getMessage(), ex);
+        log.error("Resource not found: {}", ex.getMessage(), ex);
         CustomErrorResponse errorResponse = new CustomErrorResponse("NOT_FOUND", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<CustomErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
-        logger.error("Invalid input: {}", ex.getMessage(), ex);
+        log.error("Invalid input: {}", ex.getMessage(), ex);
         CustomErrorResponse errorResponse = new CustomErrorResponse("BAD_REQUEST", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<CustomErrorResponse> handleNoSuchElementException(NoSuchElementException ex) {
-        logger.error("Element not found: {}", ex.getMessage(), ex);
+        log.error("Element not found: {}", ex.getMessage(), ex);
         CustomErrorResponse errorResponse = new CustomErrorResponse("NOT_FOUND", "The requested element was not found.");
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<CustomErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
-        logger.error("Validation error: {}", ex.getMessage(), ex);
+        log.error("Validation error: {}", ex.getMessage(), ex);
         String errorMessage = ex.getConstraintViolations().iterator().next().getMessage();
         CustomErrorResponse errorResponse = new CustomErrorResponse("VALIDATION_ERROR", errorMessage);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
@@ -53,7 +51,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<CustomErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        logger.error("Type mismatch in request parameter: {}", ex.getMessage(), ex);
+        log.error("Type mismatch in request parameter: {}", ex.getMessage(), ex);
         CustomErrorResponse errorResponse = new CustomErrorResponse("BAD_REQUEST", "Invalid type for parameter: " + ex.getName());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
